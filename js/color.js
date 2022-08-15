dc343f6574f7a0e236edc1becf93b87095722ac2
SsNiPeR1 <63201833+SsNiPeR1@users.noreply.github.com>
2022-08-15 10:58:05 +0200

    made animation somehow slower and removed duplicate colors

--- js/color.js
+++ js/color.js
@@ -2,6 +2,10 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 async function color() {
     var _COLORS = [
         "#9400D3",
@@ -17,12 +21,11 @@ async function color() {
         "#008000",
         "#808000",
         "#800080",
-        "#00FF00",
-        "#00FFFF",
     ];
+    document.getElementById("name").style.color = _COLORS[getRandomInt(12)];
     for (; ;) {
         for (var i = 0; i < _COLORS.length; i++) {
-            await sleep(400);
+            await sleep(1000);
             document.getElementById("name").style.color = _COLORS[i];
         }
     }
